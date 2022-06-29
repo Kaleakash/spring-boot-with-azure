3675b679c973c288599e9215ee25f7dddbd7b713
akash <akash300383@gmail.com>
2022-06-29 09:12:47 +0530

    done

--- src/main/java/com/controller/SampleController.java
+++ src/main/java/com/controller/SampleController.java
@@ -3,8 +3,10 @@ package com.controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @RequestMapping("azure")
+@RestController
 public class SampleController {
 
 	@GetMapping(value = "say/{name}")
